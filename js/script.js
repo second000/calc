9faeb4648eb9c2349b6cd8c0174f4623a1bedda1
Yulya Vorontsova <secondnumber01@gmail.com>
2019-08-09 17:27:53 +0300

    A bit of changes in functions of calculator

--- js/script.js
+++ js/script.js
@@ -1,13 +1,75 @@
-'use strict'
-let buttons= document.querySelectorAll('button');
-let display = document.getElementById('display');
+// 'use strict'
+// let buttons= document.querySelectorAll('button');
+// let display = document.getElementById('display');
+//
+// let userClick = function(el) {
+//   let value = el.target.textContent;
+//   display.value = value;
+// }
+//
+// for (let i = 0; i < buttons.length; i++) {
+//   let button = buttons[i];
+//   button.addEventListener('click', userClick);
+// }
+let numbers = document.querySelectorAll('.number');
+let operations = document.querySelectorAll('.operation');
+let decimalBtn = document.getElementById('decimal');
+let ce = document.getElementById('ce');
+let c = document.getElementById('c');
+let result = document.getElementById('result');
+let howWorkBtn = document.getElementById('howWorkBtn');
 
-let userClick = function(el) {
-  let value = el.target.textContent;
-  display.value = value;
-}
 
-for (let i = 0; i < buttons.length; i++) {
-  let button = buttons[i];
-  button.addEventListener('click', userClick);
-}
+for (let i=0; i<numbers.length; i++) {
+  let number = numbers[i];
+  number.addEventListener('click', function (e) {
+    console.log('!');
+});
+};
+
+for (let i=0; i<operations.length; i++) {
+  let operation = operations[i];
+  operation.addEventListener('click', function (e) {
+    console.log('!');
+});
+};
+
+decimalBtn.addEventListener('click', function (e) {
+  console.log('!');
+});
+
+ce.addEventListener('click', function (e) {
+  console.log('!');
+});
+
+c.addEventListener('click', function (e) {
+  console.log('!');
+});
+
+result.addEventListener('click', function (e) {
+  console.log('!');
+});
+
+howWorkBtn.addEventListener('click', function (e) {
+  console.log('!');
+});
+
+function numberPress() {
+
+};
+
+function operation(arg) {
+
+};
+
+function decimal(arg) {
+
+};
+
+function clear(arg) {
+
+};
+
+function howWork(arg) {
+
+};
